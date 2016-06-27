f5b49c1c31e901daec2417d74ea18ee7be732d14
andersonledo <anderson.ledo@gmail.com>
2016-06-26 21:44:03 -0300

    improvment in create-project script

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -18,5 +18,8 @@ MODELS_PATH="../model"
 PROJECTS_PATH="../projects"
 
 cp -r $MODELS_PATH/project $PROJECTS_PATH/$project_name
+echo "$project_name" > $PROJECTS_PATH/$project_name/nome-projeto.info
+
+echo "Projeto $project_name criado com sucesso na pasta $PROJECTS_PATH."
 
 exit 0
