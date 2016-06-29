fa6b6b4d6713af1b08ae733a2ce479e201e2ff96
Anderson Ledo <anderson.ledo@gmail.com>
2016-06-28 23:28:41 -0300

    improving create-project script.

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -15,9 +15,20 @@ project_name=`basename $1`
 
 
 MODELS_PATH="../model"
-PROJECTS_PATH="../projects"
 
-cp -r $MODELS_PATH/project $PROJECTS_PATH/$project_name
+PROJECTS_PATH="../projects"
+#checks if the path 'projects' exists and creates it otherwise.
+if [ ! -d "$PROJECTS_PATH" ]; then
+  mkdir "$PROJECTS_PATH"
+fi
+
+NEW_PROJECT="$PROJECTS_PATH/$project_name"
+if [ -d "$NEW_PROJECT" ]; then
+  echo "Project $project_name already exists."
+  exit 1 #choose a correct error code
+fi 
+
+cp -r $MODELS_PATH/project $NEW_PROJECT
 echo "$project_name" > $PROJECTS_PATH/$project_name/nome-projeto.info
 
 echo "Projeto $project_name criado com sucesso na pasta $PROJECTS_PATH."
